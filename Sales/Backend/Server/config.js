17dcd53f8b28c598e21eb437f3fab39fceb8fc33
mdarifulislam08 <md.arifulislamofficial08@gmail.com>
2025-05-19 17:37:31 +0600

    Update config.js

--- Sales/Backend/Server/config.js
+++ Sales/Backend/Server/config.js
@@ -1,10 +1,28 @@
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
 
-// Create Sequelize instance
-const sequelize = new Sequelize('salesforce', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false
-});
+// // Create Sequelize instance
+// const sequelize = new Sequelize('salesforce', 'root', '', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   logging: false
+// });
+
+// module.exports = sequelize;
+
+
+
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME || "salesforce",
+  process.env.DB_USER || "root",
+  process.env.DB_PASS || "",
+  {
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
